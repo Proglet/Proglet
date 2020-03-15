6b5613d9495ba61793e69c2b75db775f297f3b41
Johan Talboom <borfje@gmail.com>
2020-03-16 00:06:02 +0100

    Added a multipart post method for exercise submissions

--- API/Controllers/ExercisesController.cs
+++ API/Controllers/ExercisesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CoreDataORM;
 using Proglet.Core.Data;
+using API.Models.Multipart;
 
 namespace API.Controllers
 {
@@ -19,6 +20,16 @@ namespace API.Controllers
         public ExercisesController(DataContext context)
         {
             _context = context;
+        }
+
+
+        [HttpPost("Submit")]
+        public IActionResult Submit([FromForm] ExerciseSubmission submission)
+        {
+            Console.WriteLine(submission.CourseId);
+            Console.WriteLine(submission.ExerciseName);
+            Console.WriteLine(submission.Data.FileName);
+            return Ok();
         }
 
 
---
+++ API/Models/Multipart/ExerciseSubmission.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models.Multipart
+{
+    public class ExerciseSubmission
+    {
+        public int CourseId { get; set; }
+        public string ExerciseName { get; set; }
+        public IFormFile Data { get; set; }
+    }
+}
